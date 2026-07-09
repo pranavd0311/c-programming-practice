e78c2d640950ee41707848c5ab4828eb3017bb33
Pranav D <pranavd0311@gmail.com>
2026-07-09 22:29:31 +0530

    Add arithmetic operators program

---
+++ 02-operators/arithmetic-operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 20, b = 6;
+
+    printf("Addition: %d\n", a + b);
+    printf("Subtraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+    printf("Remainder: %d\n", a % b);
+
+    return 0;
+}
